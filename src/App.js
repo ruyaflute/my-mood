2598ab47352abd699f620f4aa610848a2aaf49a2
nancyruya <nancyruya@gmail.com>
2020-05-09 12:05:07 -0400

    add background-cloud

--- src/App.js
+++ src/App.js
@@ -14,25 +14,31 @@ function App() {
         Show me how you feel, I will show you the weather.
         </p>
 
-      <form className="Form">
-        <lable>
-          My Mood:
-          <input
-          className="Loginput"
-            type="text"
-            id="log"
-            placeholder="How am I feeling right now..."
-          />
-        </lable>
+      <form className="Form" id="Moodentry">
+      <textarea 
+      id="log" 
+      cols="40" 
+      rows="5" 
+      placeholder="How am I feeling right now...">
+      </textarea>
+          
         <a
           className="App-link"
           href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Submit
+          Save
         </a>
       </form>
+
+      <div id="clouds">
+	<div class="cloud x1"></div>
+	<div class="cloud x2"></div>
+	<div class="cloud x3"></div>
+	<div class="cloud x4"></div>
+	<div class="cloud x5"></div>
+</div>
 
 
     </div>

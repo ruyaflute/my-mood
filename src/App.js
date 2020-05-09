5defc47999364a6fe4460ea750b399a492d728c2
nancyruya <nancyruya@gmail.com>
2020-05-09 10:47:24 -0400

    page layout, need to: moving background, input not going to the second line, submit link to where

--- src/App.js
+++ src/App.js
@@ -3,22 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      {/*<header className="App-header"> </header>
+        <img src={logo} className="App-logo" alt="logo" /> */}
+      <h2>Welcome to My Mood</h2>
+
+      <p className="Subheader">
+        Show me how you feel, I will show you the weather.
         </p>
+
+      <form className="Form">
+        <lable>
+          My Mood:
+          <input
+          className="Loginput"
+            type="text"
+            id="log"
+            placeholder="How am I feeling right now..."
+          />
+        </lable>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Submit
         </a>
-      </header>
+      </form>
+
+
     </div>
   );
 }

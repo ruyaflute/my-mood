ac9f6ebbbeae4f19cf31eb87d73779574e6da599
nancyruya <nancyruya@gmail.com>
2020-05-09 22:11:27 -0400

    battling with save button

--- src/App.js
+++ src/App.js
@@ -2,22 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
- {/*
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      mood: "" 
+      value: '' 
+};
+this.handleChange = this.handleChange.bind(this);
+this.handleSubmit = this.handleSubmit.bind(this);
 }
-};
 
-saveChange = () => {
-  this.setState(() => ({
-    mood: text
-  }))
-};
-*/}
+handleChange(event) {
+  this.setState({value: event.target.value});
+}
 
+handleSubmit(event) {
+  alert('An essay was submitted: ' + this.state.value);
+  event.preventDefault();
+}
+
+
+render() {
 
   return (
     <div className="App">
@@ -29,21 +34,25 @@ saveChange = () => {
         Show me how you feel, I will show you the weather.
         </p>
 
-      <form className="Form" id="Moodentry">
+      <form onSubmit={this.handleSubmit}
+      className="Form" id="Moodentry">
         <textarea
           id="textArea"
           cols="40"
           rows="5"
           placeholder="How am I feeling right now...">
+            value={this.state.value} onChange={this.handleChange}
         </textarea>
 
+        <input type="submit" value="Submit" />
+        {/*
         <button 
         type="button" 
         value="save"
         className="Save-button"  
-        /* onClick = { this.saveChange } */
-        > Save</button>
-      </form>
+        onClick = { this.saveChange } 
+        > Save</button>*/}
+        </form> 
 
       <div id="clouds">
         <div class="cloud x1"></div>
@@ -52,10 +61,9 @@ saveChange = () => {
         <div class="cloud x4"></div>
         <div class="cloud x5"></div>
       </div>
-
-
     </div>
   );
+}
 }
 
 export default App;
